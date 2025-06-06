d17704fea1146a32563dcfe2d3d595b9df03bd13
Laurin Koller <laurin.koller@gmail.com>
2025-06-06 11:08:10 +0200

    Update simple_utils.py
    Add transform_columns function, that uses a yeo power transformer to change the distribution of given features similar to a normal distribution

--- other/simple_utils.py
+++ other/simple_utils.py
@@ -1,9 +1,19 @@
 import numpy as np
 import pandas as pd
 import random
+from sklearn.preprocessing import PowerTransformer
 
 # %%
 
 def set_random_seed(random_seed) -> None:
     np.random.seed(random_seed)
     random.seed(random_seed)
+    
+def transform_columns(train_df: pd.DataFrame, test_df: pd.DataFrame, columns: list):
+    yeo = PowerTransformer(method='yeo-johnson')
+    
+    for col in columns:
+        train_df[col] = yeo.fit_transform(np.array(train_df[col]))
+        test_df[col] = yeo.transform(np.array(test_df[col]))
+    
+    return train_df, test_df
