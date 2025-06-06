b5cd18380332b7d3893dfed8163dee6dfe497b5a
Laurin Koller <laurin.koller@gmail.com>
2025-06-06 13:02:04 +0200

    Update simple_model.py

--- other/simple_model.py
+++ other/simple_model.py
@@ -17,8 +17,130 @@ molecular_file_test = data_dir+'\\X_test\\molecular_test.csv'  # Molecular test 
 # Features from the clinical data to include in the model
 clinical_features = ['BM_BLAST', 'HB', 'PLT', 'WBC', 'ANC', 'MONOCYTES']
 
+import os
+os.chdir(data_dir)
+
+# Import utility functions from model_rsf_utils
+#import other.model_rsf_utils2 as u
+import other.simple_utils as u
+
+# Set random seed for reproducibility
+random_seed = 1
+u.set_random_seed(random_seed)
+
 # %%
 
-status_df_original = pd.read_csv(status_file) # shape (3323, 3)
-clinical_df_original = pd.read_csv(clinical_file) # shape (3323, 9)
-molecular_df_original = pd.read_csv(molecular_file) # shape (10935, 11)
+status_df = pd.read_csv(status_file) # shape (3323, 3)
+clinical_df = pd.read_csv(clinical_file) # shape (3323, 9)
+molecular_df = pd.read_csv(molecular_file) # shape (10935, 11)
+
+status_df = status_df.dropna(subset=["OS_YEARS", "OS_STATUS"])
+patient_ids = list(status_df['ID'])
+clinical_df = clinical_df[[True if val in patient_ids else False for val in clinical_df['ID']]]
+molecular_df = molecular_df[[True if val in patient_ids else False for val in molecular_df['ID']]]
+
+clinical_df_test = pd.read_csv(clinical_file_test)
+molecular_df_test = pd.read_csv(molecular_file_test)
+
+# %%
+
+clinical_df, clinical_df_test = u.transform_columns(clinical_df, clinical_df_test, ['BM_BLAST', 'WBC', 'ANC', 'MONOCYTES', 'PLT'])
+clinical_df, clinical_df_test = u.fill_nan(clinical_df, clinical_df_test, ['BM_BLAST', 'WBC', 'ANC', 'MONOCYTES', 'HB', 'PLT'], method='mean')
+
+cyto_train, cyto_test = u.cyto_patient_risk(clinical_df['CYTOGENETICS'], clinical_df_test['CYTOGENETICS'])
+clinical_df = pd.concat([clinical_df, cyto_train], axis=1)
+clinical_df_test = pd.concat([clinical_df_test, cyto_test], axis=1)
+
+gender_train, gender_test = u.patient_gender(clinical_df['CYTOGENETICS'], clinical_df_test['CYTOGENETICS'])
+clinical_df = pd.concat([clinical_df, gender_train], axis=1)
+clinical_df_test = pd.concat([clinical_df_test, gender_test], axis=1)
+
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
