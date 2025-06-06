f3b9f05b4ba60d6739d77acc19d326762e0a15b7
Laurin Koller <laurin.koller@gmail.com>
2025-06-06 11:09:47 +0200

    Update simple_utils.py
    fixed bug in transform_columns function

--- other/simple_utils.py
+++ other/simple_utils.py
@@ -9,11 +9,14 @@ def set_random_seed(random_seed) -> None:
     np.random.seed(random_seed)
     random.seed(random_seed)
     
+def fill_nan():
+    return
+    
 def transform_columns(train_df: pd.DataFrame, test_df: pd.DataFrame, columns: list):
     yeo = PowerTransformer(method='yeo-johnson')
     
     for col in columns:
-        train_df[col] = yeo.fit_transform(np.array(train_df[col]))
-        test_df[col] = yeo.transform(np.array(test_df[col]))
+        train_df[col] = yeo.fit_transform(np.array(train_df[col]).reshape(-1,1)).flatten()
+        test_df[col] = yeo.transform(np.array(test_df[col]).reshape(-1,1)).flatten()
     
     return train_df, test_df
